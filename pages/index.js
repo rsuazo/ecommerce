b16cfd8cac4d6eaa617d2fbf8b54b8fe55199af8
rsuazo <rsuazo2@gmail.com>
2024-12-24 17:01:33 -0600

    Initial Sanity CMS config

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,35 @@
 import React from "react";
 
-const Home = () => {
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <div>
+      <HeroBanner heroBanner={bannerData[0]} />
+
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
-      Footer
+      <div className="products-container">
+        {products?.map((product) => product.name)}
+      </div>
+      <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
